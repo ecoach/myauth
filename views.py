d31230533c1eb8ffa03c902fa422338e874dd1f0
jaredtritz <jtritz@umich.edu>
2014-01-17 16:41:10 -0500

    login/out stuff belongs here

--- views.py
+++ views.py
@@ -5,4 +5,5 @@ from django.shortcuts import render_to_response, render, redirect
 def test_view(request):
     return render(request, 'transfer/base_transfer.html', {})
 
-     
+def mylogout_view(request):
+    return HttpResponseRedirect("https://weblogin.umich.edu/cgi-bin/logout")
